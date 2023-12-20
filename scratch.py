0f1ee6942fd26a2485f023bb613a828884b4320d
jonatha-n <mwoodprop@gmail.com>
2023-12-19 18:08:23 -0600

    a

--- scratch.py
+++ scratch.py
@@ -1,1 +1,37 @@
 #test
+def solution(start, length):
+    # Your code here
+    
+    s = length * length
+    
+    if length == 1:
+        return start
+        
+    c = 0
+    threshold = length
+    diff = 0
+    i = 0
+    res = None
+    while i < s:
+        if c == threshold:
+            while diff != 0:
+                diff -= 1
+                i+=1
+                continue
+            threshold = threshold - 1
+            diff = length - threshold
+            c = 0
+        curr = start + i
+        if curr > start + (length * (length - 1)):
+            break
+        c += 1
+        i += 1
+        if res == None:
+            res = curr
+        else:
+            res = res^curr
+    return res
+
+x = solution(0, 1)
+print(x)
+print(1^2)
